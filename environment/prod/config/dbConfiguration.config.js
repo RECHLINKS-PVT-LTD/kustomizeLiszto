9dab033d2b08818a900cde08a71dd7213bcd2a1f
Santhosh <santhosh@rechlinks.com>
2022-12-21 14:36:47 +0000

    First Commit

---
+++ environment/prod/config/dbConfiguration.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+      'secret': 'restapisecret',
+      'database': 'mongodb://10.110.0.141:27017,10.110.0.140:27017/lisztodata?replicaSet=rs0'
+    };
---
+++ environment/prod/config/ipConfiguration.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+      publicIp: "73.151.133.9",
+      applicationUrl: "http://127.0.0.1:5000/",
+      applicationPort: "5000",
+
+      filesServerUrl: "http://127.0.0.1:5000/",
+
+      icecastIp: "icecast.rechlinks.com",
+      icecastUrl: "http://icecast.rechlinks.com:8000/",
+      icecastPort: "8000",
+      icecastUserName: "admin",
+      icecastPassword: "connect",
+
+      kafkaHost: "kafka-service:9092",
+
+      redisHost: "redis-service",
+      redisPort: "6379",
+
+      float_long_url: "http://73.151.133.9:1002/#/user/wall/float;id=",
+    };
